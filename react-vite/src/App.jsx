feb7fcace3ff6c27dfeb04a225b265cec507b92e
Batuhan Semiz <batuhansemiz52@gmail.com>
2023-08-19 20:04:02 +0300

    bazı değerler eklendi

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,34 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ const students = 430000;
+ const dogruMu = true;
+ const date = 'date';
+ const url = 'https://picsum.photos/250/250';
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+    <div>
+      <p>batuhan</p>
+      <p>{students }</p>
+      <p>Doğru mu ?  :{dogruMu}</p>
+      {dogruMu ? <p>öğrenci sayyısı</p> : <p>kurs sayısı</p> }
+      <input type={date} />
+      <img src={url} alt="" />
+      <div className='box'></div>
+      <div style={{
+        width:'200px',
+        height: '200px',
+        backgroundColor: 'red',
+      }}></div>
+    </div>
   )
 }
 
